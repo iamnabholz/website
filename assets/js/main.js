edddaf23356eae809d3a91cfdccf87d63cdf5e64
Lukas Nabholz <iamnabholz@gmail.com>
2021-02-09 18:02:36 -0400

    added footer and fixed some styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,14 +7,23 @@ function openMenu() {
     if (opened) {
         opened = false;
         document.getElementById("header-title").style.color = "#0F245B";
+        document.querySelector("body").style.overflow = 'visible';
     } else {
         opened = true;
         document.getElementById("header-title").style.color = "white";
+        document.querySelector("body").style.overflow = 'hidden';
     }
 }
 
-function copyEmail() {
-    navigator.clipboard.writeText("lukas@nabholz.work")
+function copyEmail(element) {
+    var noti = document.getElementById("noti");
+    noti.style.display = "block";
+    noti.style.left = element.offsetLeft + "px";
+    noti.style.top = element.offsetTop + (element.clientHeight * 1.2) + "px";
+    navigator.clipboard.writeText("lukas@nabholz.work");
+    setTimeout(function () {
+        noti.style.display = "none";
+    }, 1200);
 }
 
 if (window.location.pathname == "/") {
