497a7fd3fef2f17711eb470dff405d8832a54345
Lukas Nabholz <iamnabholz@gmail.com>
2022-05-20 10:35:36 -0400

    almost final design

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const now = new Date();
+
+const current = now.toLocaleTimeString('default', {
+  timeZone: "America/La_Paz",
+  // en-US can be set to 'default' to use user's browser settings
+  hour: 'numeric',
+  minute: '2-digit',
+});
+
+document.getElementById("current-time").innerHTML = current;
