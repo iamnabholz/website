efeb04f1e55535f619636c532df9403d88efc131
Lukas Nabholz <iamnabholz@gmail.com>
2022-10-05 20:40:25 -0400

    release candidate

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,39 @@ document.getElementById("current-time").innerHTML = current;
 // HIDE ARROW INDICATOR ON SCROLL
 window.addEventListener('scroll', () => {
   if (window.scrollY > 30) {
-      document.getElementById('work').classList.add("hide");
+    document.getElementById('work').classList.add("hide");
   } else {
-      document.getElementById('work').classList.remove("hide");
+    document.getElementById('work').classList.remove("hide");
   }
+
+  elementInViewport();
 });
+
+window.addEventListener('load', () => {
+  elementInViewport();
+})
+
+var elements = document.getElementsByClassName("appear");
+
+function elementInViewport(value) {
+  for (let i = 0; i < elements.length; i++) {
+    const myElement = elements[i];
+    var bounding = myElement.getBoundingClientRect();
+    var myElementHeight = (myElement.offsetHeight -= 120);
+    var myElementWidth = myElement.offsetWidth;
+
+    var bounding = myElement.getBoundingClientRect();
+
+    if (bounding.top >= -myElementHeight
+      && bounding.left >= -myElementWidth
+      && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + myElementWidth
+      && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
+
+      myElement.classList.remove("hidden");
+    } else {
+      if (window.scrollY < bounding.top) {
+        myElement.classList.add("hidden");
+      }
+    }
+  }
+}
